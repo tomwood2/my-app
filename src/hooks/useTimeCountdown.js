c0623379003d03ccb4eda31519f90d771457c4a5
tomwood2@gmail.com <tomwood2@gmail.com>
2022-09-27 20:00:03 -0700

    Started work on documentation

--- src/hooks/useTimeCountdown.js
+++ src/hooks/useTimeCountdown.js
@@ -1,6 +1,38 @@
 
 import {useState, useEffect} from 'react';
 
+/*
+returns 2 methods and 2 state variables
+
+startCountdown()
+starts counting down from ticks to 0 for
+    each millisecond.  Sets countdownCompleted
+    when currentCount reaches 0
+
+cancelCountdown
+cancels the countdown if it is running and 
+    sets currentTick to -1
+
+countdownCompleted:
+initially set to false
+is set to false when startCountdown is called
+is set to true when a count down completes
+normally - currentTick reaches 0.  It remains set to true
+until startCountdown or cancelCountdown is called.
+
+currentTick
+initially set to -1
+when startCoundown is called it is set to the ticks
+    argument provided and starts a timer
+when timer expires it is decremented and a new timer is
+    started as long as it is still positive.  When it
+    decements to 0, no more timers are started.  it
+    remains set to 0
+when cancelCountdown is called currentTick is set to -1
+    and any timer is cancelled.
+*/
+
+
 function useTimeCountdown() {
 
     const [millisecondsPerTick, setMillisecondsPerTick] = useState(0);
